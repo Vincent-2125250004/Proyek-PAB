2783adccf3aa09a529bbb90d565863d7d3f4f6cc
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2022-12-12 21:21:06 +0700

    Proyek PAB

--- app/src/main/java/com/if3a/paimonopedia/activity/MainActivity.java
+++ app/src/main/java/com/if3a/paimonopedia/activity/MainActivity.java
@@ -71,5 +71,19 @@ public class MainActivity extends AppCompatActivity {
         FT.commit();
     }
 
+    @Override
+    public void onBackPressed() {
+
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+            //additional code
+        } else {
+            getSupportFragmentManager().popBackStack();
+        }
+
+    }
+
 
 }
